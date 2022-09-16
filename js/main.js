616630ca2e12756ee1941b86deca0a9956a8e20a
james <jamesattfield1990@gmail.com>
2022-09-16 19:35:09 +0100

    updated projects, cleaned up classes/css

--- js/main.js
+++ js/main.js
@@ -2,11 +2,9 @@ $(document).ready(function(){
 	$(".showcase-section").hover(
 		function(){
 			$(this).addClass("shadow-lg");
-			$(this).animate({"marginTop": "-=0.2em"});
 		}, 
 		function(){
 			$(this).removeClass("shadow-lg");
-			$(this).animate({"marginTop": "+=0.2em"});
 		}
 	);
 });
