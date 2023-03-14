e1349afc994d8746de77383282bb4b26d27b11eb
James Attfield <jamesattfield98@gmail.com>
2023-03-14 18:28:12 +0000

    editted hero card text, added inline spans for languages

--- Blazor/Blazor/tailwind.config.js
+++ Blazor/Blazor/tailwind.config.js
@@ -1,23 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./**/*.{razor,html,cshtml}"],
-  theme: {
-      extend: {
-          colors: {
-              "accent": "#fd1b7b",
-              "hero": "#97DFFC",
-              "secondary": "#002866",
-              "light": "#FFFFFF",
+    theme: {
+        extend: {
+            colors: {
+                "accent": "#fd1b7b",
+                "hero": "#97DFFC",
+                "secondary": "#002866",
+                "light": "#FFFFFF",
 
-              "deep-blue": "#000836",
+                "deep-blue": "#000836",
 
-              "ruby": "#E0115F",
-              "csharp": "#9c75d5",
-              "mysql": "#dd8a00",
-              "php": "#7377ad",
-              "java": "#507e9c"
-          }
-      },
-  },
-  plugins: [],
+                "ruby": "#E0115F",
+                "csharp": "#9c75d5",
+                "mysql": "#dd8a00",
+                "php": "#7377ad",
+                "java": "#507e9c"
+            }
+        },
+    },
+    plugins: [],
+    safelist: [
+        {
+            pattern: /.+-(ruby|csharp|mysql|php|java)/,
+            variants: [],      // Optional
+        },
+    ],
 }
