3d94c5a9a017d37915e5bd2bf83a03b32c47cdbd
James Attfield <jamesattfield98@gmail.com>
2023-04-17 22:46:49 +0100

    added /cloutshoots

---
+++ Blazor/Blazor/Models/CloutShoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Blazor.Models
+{
+    public class CloutShoot
+    {
+        [JsonIgnore]
+        public Guid Guid { get; set; }
+
+        public string Name { get; set; }
+
+        [JsonIgnore]
+        public string NormalisedName { get => Name.Normalize(); }
+
+        public DateTime Date { get; set; }
+
+        public string? MapUrl { get; set; }
+        public string? FormUrl { get; set; }
+    }
+}
