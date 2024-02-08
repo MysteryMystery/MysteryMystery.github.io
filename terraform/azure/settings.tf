3ab10a4abecb4d0ce24031a03b92c9390a71f327
james <jamesattfield1990@gmail.com>
2024-02-08 20:44:12 +0000

    updated azurerm config to azure

--- terraform/azure/settings.tf
+++ terraform/azure/settings.tf
@@ -19,4 +19,7 @@ provider "azurerm" {
   features {}
 
   subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  use_oidc        = true
 }
--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -6,5 +6,10 @@ variable "environment" {
 }
 
 variable "subscription_id" {
+}
 
+variable "tenant_id" {
+}
+
+variable "client_id" {
 }
