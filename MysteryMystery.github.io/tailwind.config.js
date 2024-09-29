f746602d4b630b3c97b419f23420ac69b0e6bf56
James Attfield <jamesattfield98@gmail.com>
2024-09-29 21:22:14 +0100

    added avatars

--- MysteryMystery.github.io/tailwind.config.js
+++ MysteryMystery.github.io/tailwind.config.js
@@ -8,41 +8,41 @@ module.exports = {
     ],
     theme: {
         extend: {
-          colors: {
-            dark: '#1D1E18',
-            light: '#edf0ee',
-    
-            primary: {
-              light: '#005377',
-              dark: '#01344a',
+            colors: {
+                dark: '#1D1E18',
+                light: '#edf0ee',
+
+                primary: {
+                    light: '#005377',
+                    dark: '#01344a',
+                },
+                secondary: {
+                    light: '#83B5D1',
+                    dark: '#83B5D1',
+                },
+                pokemon: {
+                    normal: '#a0a0a0',
+                    fire: '#ff4500',
+                    water: '#0074d9',
+                    electric: '#ffdc00',
+                    grass: '#4caf50',
+                    ice: '#b3e0ff',
+                    fighting: '#d32f2f',
+                    poison: '#9c27b0',
+                    ground: '#795548',
+                    flying: '#03a9f4',
+                    psychic: '#e91e63',
+                    bug: '#8bc34a',
+                    rock: '#9e9e9e',
+                    ghost: '#673ab7',
+                    dark: '#212121',
+                    steel: '#607d8b',
+                    fairy: '#ff80ab',
+                    dragon: '#ff5722',
+                }
             },
-            secondary: {
-              light: '#8EE3EF',
-              dark: '#66a5ad',
-            },
-            pokemon: {
-                normal: '#a0a0a0',
-                fire: '#ff4500',
-                water: '#0074d9',
-                electric: '#ffdc00',
-                grass: '#4caf50',
-                ice: '#b3e0ff',
-                fighting: '#d32f2f',
-                poison: '#9c27b0',
-                ground: '#795548',
-                flying: '#03a9f4',
-                psychic: '#e91e63',
-                bug: '#8bc34a',
-                rock: '#9e9e9e',
-                ghost: '#673ab7',
-                dark: '#212121',
-                steel: '#607d8b',
-                fairy: '#ff80ab',
-                dragon: '#ff5722',
-            }
-          },
         },
-      },
+    },
     plugins: [],
 }
 
