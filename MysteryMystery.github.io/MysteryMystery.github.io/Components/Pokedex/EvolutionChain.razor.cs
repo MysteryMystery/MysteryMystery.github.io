6f6f7c34ac132196dff1fc0702ffe28511c1c528
james <jamesattfield1990@gmail.com>
2024-10-02 19:17:17 +0100

    move evo chain to component

---
+++ MysteryMystery.github.io/MysteryMystery.github.io/Components/Pokedex/EvolutionChain.razor.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Components;
+using MysteryMystery.github.io.Models.Pokedex;
+using MysteryMystery.github.io.Repositories.Pokedex;
+
+namespace MysteryMystery.github.io.Components.Pokedex
+{
+    public partial class EvolutionChain : ComponentBase
+    {
+        [Parameter]
+        public required Pokemon Pokemon { get; set; }
+
+        [Inject]
+        public IPokemonRepository PokemonRepository { get; set; }
+
+        [Inject]
+        public ILogger<EvolutionChain> Logger { get; set; }
+
+        private List<Pokemon?> _evolutionChain = new List<Pokemon?>();
+
+        protected async override Task OnParametersSetAsync()
+        {
+            Logger.LogInformation($"{nameof(Pokemon)}: {Pokemon.Name}");
+
+            _evolutionChain = await PopulateEvolutionChain();
+        }
+
+        private async Task<List<Pokemon?>> PopulateEvolutionChain()
+        {
+            List<Pokemon?> evoChain = new List<Pokemon?>();
+
+            PokemonSpecies species = await PokemonRepository.GetApiResource<PokemonSpecies>(Pokemon.Species);
+            PokemonEvolutionChain chain = await PokemonRepository.GetApiResource<PokemonEvolutionChain>(species.EvolutionChain);
+            return await PopulateEvolutionChainLink(evoChain, chain.Chain);
+        }
+
+        private async Task<List<Pokemon?>> PopulateEvolutionChainLink(List<Pokemon?> chain, params PokemonEvolutionChainLink[] links)
+        {
+            foreach (var link in links)
+            {
+                PokemonSpecies species = await PokemonRepository.GetApiResource<PokemonSpecies>(link.Species);
+                Pokemon? pokemon = Pokemon?.Id == species.Id ? Pokemon : await PokemonRepository.GetPokemonAsync(species.Id);
+                chain.Add(pokemon);
+                if (link.EvolvesTo == null)
+                {
+                    continue;
+                }
+                return await PopulateEvolutionChainLink(chain, link.EvolvesTo.ToArray());
+            }
+            return chain;
+        }
+    }
+}
--- MysteryMystery.github.io/MysteryMystery.github.io/Pages/Pokedex/PokemonShow.razor.cs
+++ MysteryMystery.github.io/MysteryMystery.github.io/Pages/Pokedex/PokemonShow.razor.cs
@@ -20,9 +20,6 @@ namespace MysteryMystery.github.io.Pages.Pokedex
         public NavigationManager NavigationManager { get; set; }
 
         [Inject]
-        public HttpClient HttpClient { get; set; }
-
-        [Inject]
         public IPokemonRepository PokemonRepository { get; set; }
 
         [Parameter]
@@ -31,8 +28,6 @@ namespace MysteryMystery.github.io.Pages.Pokedex
         private Pokemon? _pokemon { get; set; }
 
         private ListResponse<NamedAPIResource>? _pokemonResponse = null;
-
-        private List<Pokemon?> _evolutionChain = new List<Pokemon>();
 
         private bool _isLoading = true;
 
@@ -51,7 +46,6 @@ namespace MysteryMystery.github.io.Pages.Pokedex
             _isLoading = true;
 
             _pokemon = await PokemonRepository.GetPokemonAsync(NameOrId);
-            await PopulateEvolutionChain();
 
             if (_pokemon is null)
             {
@@ -76,29 +70,6 @@ namespace MysteryMystery.github.io.Pages.Pokedex
             .Where(x => x != null)
             .Select(x => x!)
             .ToList();
-        }
-
-        private async Task PopulateEvolutionChain()
-        {
-            _evolutionChain.Clear();
-            PokemonSpecies species = await PokemonRepository.GetApiResource<PokemonSpecies>(_pokemon.Species);
-            PokemonEvolutionChain chain = await PokemonRepository.GetApiResource<PokemonEvolutionChain>(species.EvolutionChain);
-            await PopulateEvolutionChainLink(chain.Chain);
-        }
-
-        private async Task PopulateEvolutionChainLink(params PokemonEvolutionChainLink[] links)
-        {
-            foreach(var link in links)
-            {
-                PokemonSpecies species = await PokemonRepository.GetApiResource<PokemonSpecies>(link.Species);
-                Pokemon? pokemon = _pokemon?.Id == species.Id ? _pokemon : await PokemonRepository.GetPokemonAsync(species.Id);
-                _evolutionChain.Add(pokemon);
-                if (link.EvolvesTo == null)
-                {
-                    continue;
-                }
-                await PopulateEvolutionChainLink(link.EvolvesTo.ToArray());
-            }
         }
     }
 }
