16b57815da0a00ca6512e98b1607fb4454146557
James Attfield <jamesattfield98@gmail.com>
2024-12-07 13:20:14 +0000

    redesign of landing page

---
+++ MysteryMystery.github.io/MysteryMystery.github.io/Components/Timeline/TimeLine.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MysteryMystery.github.io.Components.Timeline
+{
+    public partial class TimeLine : ComponentBase
+    {
+        [Parameter]
+        public required RenderFragment ChildContent { get; set; }
+    }
+}
---
+++ MysteryMystery.github.io/MysteryMystery.github.io/Components/Timeline/TimeLineItem.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MysteryMystery.github.io.Components.Timeline
+{
+    public partial class TimeLineItem : ComponentBase
+    {
+        [Parameter]
+        public required RenderFragment Icon { get; set; }
+
+        [Parameter]
+        public required RenderFragment Title { get; set; }
+
+        [Parameter]
+        public RenderFragment? Description { get; set; }
+
+        [Parameter]
+        public DateOnly? Date { get; set; }
+    }
+}
