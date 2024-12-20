cf7efe820140ff0dba83aa04790addbec229045b
James Attfield <jamesattfield98@gmail.com>
2024-12-20 11:09:44 +0000

    add staging environment, fade animation on cards

---
+++ MysteryMystery.github.io/MysteryMystery.github.io/Pages/Home.razor.js
@@ -0,0 +1,30 @@
+﻿export function initializeScrollEffect() {
+    const containers = document.getElementsByClassName('hero-fade-container');
+
+    window.addEventListener('scroll', () => {
+        for (const container of containers) {
+            const children = container.getElementsByClassName('hero-fade');
+
+            for (const child of children) {
+                const rect = child.getBoundingClientRect();
+                const viewportHeight = window.innerHeight;
+                const elementCenter = rect.top + (rect.height / 2);
+                const screenCenter = viewportHeight / 2;
+                const distanceFromCenter = Math.abs(elementCenter - screenCenter);
+                const maxDistance = viewportHeight / 2;
+                const opacity = 1.2 - (distanceFromCenter / maxDistance);
+
+                let roundedOpacity = Math.abs(Math.ceil((opacity * 100) / 10) * 10);
+                if (roundedOpacity > 100)
+                    roundedOpacity = 100
+
+                try {
+                    child.classList.remove("bg-opacity-" + (roundedOpacity - 10))
+                } catch {
+                    
+                }
+                child.classList.add("bg-opacity-" + roundedOpacity)
+            }
+        }
+    });
+}
