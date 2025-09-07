eac63755c014221088d91c7280cd85cffda3854f
James Attfield <jamesattfield98@gmail.com>
2025-09-07 19:43:31 +0100

    responsiveness & put icons into components

---
+++ MysteryMystery.github.io/MysteryMystery.github.io.Components/Components/Icons/LinkedIn.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MysteryMystery.github.io.Components.Components.Icons
+{
+    public partial class LinkedIn : ComponentBase
+    {
+        [Parameter]
+        public string Class { get; set; } = "";
+
+        [Parameter]
+        public string Fill { get; set; } = "";
+    }
+}
